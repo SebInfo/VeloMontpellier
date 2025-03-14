e81c53a1bb68a689c117578ea6512fc053b92d2f
SebInfo <contact@inion.info>
2025-03-14 10:38:58 +0100

    Recupère tous les infos depuis l'API afin de faire un test de connexion à l'API Bike

---
+++ toutesLesInfos.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+# URL de l'API des stations VéloMagg
+API_URL = "https://portail-api-data.montpellier3m.fr/bikestation"
+response = requests.get(API_URL, headers={"Accept": "application/json"})
+
+# Vérifier si la requête a réussi
+if response.status_code == 200:
+    data = response.json()  # Convertir la réponse en JSON
+
+    # Afficher le JSON complet pour voir toutes les informations
+    print(json.dumps(data, indent=4))
+
+else:
+    print(f"Erreur {response.status_code}: Impossible de récupérer les données.")
