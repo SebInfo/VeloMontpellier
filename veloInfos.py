181bae39fe1f2ca53fc1854a72e652edd3fce95f
SebInfo <contact@inion.info>
2025-11-18 08:38:58 +0100

    Commit vélo info

--- veloInfos.py
+++ veloInfos.py
@@ -8,9 +8,7 @@ response = requests.get(API_URL, headers={"Accept": "application/json"})
 # Vérifier si la requête a réussi
 if response.status_code == 200:
     data = response.json()  # Convertir la réponse en JSON
-
     # Afficher le JSON complet pour voir toutes les informations
     print(json.dumps(data, indent=4))
-
 else:
     print(f"Erreur {response.status_code}: Impossible de récupérer les données.")
